2cf674773d686bb6079e1fd0fafff293163472d9
Som18 <sohomsomp@gmail.com>
2026-02-27 10:00:50 +0530

    fix: import login and register controllers

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/authController');
+const { login, register } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
